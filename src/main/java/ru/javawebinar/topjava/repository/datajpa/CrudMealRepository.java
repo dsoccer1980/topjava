d4deb6eb4014f2c833f50c572911d692e39987a2
dsoccer1980 <dsoccer1980@gmail.com>
2018-07-19 19:55:17 +0300

    Realize DataJpaMealRepositoryImpl

--- src/main/java/ru/javawebinar/topjava/repository/datajpa/CrudMealRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/datajpa/CrudMealRepository.java
@@ -1,7 +1,31 @@
 package ru.javawebinar.topjava.repository.datajpa;
 
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 import ru.javawebinar.topjava.model.Meal;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 public interface CrudMealRepository extends JpaRepository<Meal, Integer> {
+
+    @Override
+    @Transactional
+    Meal save(Meal meal);
+
+    Meal getByIdAndUserId(Integer id, Integer userId);
+
+    @Transactional
+    @Modifying
+    @Query(name = Meal.DELETE)
+    int delete(@Param("id") int id, @Param("userId") int userId);
+
+    List<Meal> findByDateTimeBetweenAndUserId(LocalDateTime startDate, LocalDateTime endDate, Integer userId, Sort sort);
+
+    List<Meal> findAllByUserId(Integer userId, Sort sort);
+
 }
